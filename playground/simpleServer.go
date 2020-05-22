7fe8f942f279dba7b00a51613bd4945194978f49
Fabio Soto <fasozu@gmail.com>
2020-05-22 20:48:29 +0000

    Siviendo la raiz en html

--- playground/simpleServer.go
+++ playground/simpleServer.go
@@ -4,25 +4,25 @@ import (
     "fmt"
     "log"
     "encoding/json"
-	
+    
     "github.com/buaazp/fasthttprouter"
     "github.com/valyala/fasthttp"
 )
 
-type Server struck {
-	Address string `json:"address"`
-	Ssl_grade string `json:"ssl_grade"`
+
+type Server struct {
+    Address string `json:"address"`
+    Ssl_grade string `json:"ssl_grade"`
     Country string `json:"country"`
     Owner string `json:"owner"`     
 }
 
 type Response struct {
-    Servers []Server `json:"servers"`
-    Servers_changed bool `json:"servers_changed"`
-	Ssl_grade string `json:"ssl_grade"`
-	Logo string `json:"logo"`
-	Is_down bool `json:"is_down"`
-    
+        Servers []Server `json:"servers"`
+        Servers_changed bool `json:"servers_changed"`
+        Ssl_grade string `json:"ssl_grade"`
+        Logo string `json:"logo"`
+        Is_down bool `json:"is_down"`    
 }
 
 func Index(ctx *fasthttp.RequestCtx) {
@@ -30,38 +30,38 @@ func Index(ctx *fasthttp.RequestCtx) {
 }
 
 func SimpleJson(ctx *fasthttp.RequestCtx) {
-
+    
 	var s1 = Server{
 		Address: "Server1",
-		Ssl_grade: "B"
-		Country: "US"
-		Owner: "Amazon.com, Inc."		
-	}
-
-	var s2 = Server{
-		Address: "Server2",
-		Ssl_grade: "A"
-		Country: "CO"
-		Owner: "Godaddy"		
-	}
-
-	var r1 = Response{
-		Servers []Server{}
-		Servers_changed: false,
 		Ssl_grade: "B",
-		Logo:  "https://server.com/icon.png"
-		Is_down: false
+		Country: "US",
+		Owner: "Amazon.com, Inc.",
 	}
 
-	r1.Servers = append(r1.Servers, s1)
-	r1.Servers = append(r1.Servers, s2)
+    var s2 = Server{
+        Address: "Server2",
+        Ssl_grade: "A",
+        Country: "CO",
+        Owner: "Godaddy",               
+    }
 
-	b, err := json.Marshal(r1)
+    var r1 = Response{
+        Servers: []Server{},
+        Servers_changed: false,
+        Ssl_grade: "B",
+        Logo:  "https://server.com/icon.png",
+        Is_down: false,
+    }
+
+    r1.Servers = append(r1.Servers, s1)
+    r1.Servers = append(r1.Servers, s2)
+
+    b, err := json.Marshal(r1)
     if err != nil {
-        fmt.Fprint(ctx, "Error %s \n", string(err))        
+        fmt.Fprintf(ctx, "Error %s \n", err.Error())        
     }else{
-		fmt.Fprint(ctx, "%s\n", string(b))        
-	}
+        fmt.Fprint(ctx, string(b))        
+    }
 }
 
 
@@ -70,8 +70,14 @@ func Hello(ctx *fasthttp.RequestCtx) {
 }
 
 func main() {
+
     router := fasthttprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
+        
+    router.GET("/api", Index)
+    router.GET("/api/hello/:name", Hello)
+    router.GET("/api/simplejson/:name", SimpleJson)
+
+    router.NotFound = fasthttp.FSHandler("./static", 0)
+        
     log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
